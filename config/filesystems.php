c0aadcba959365f17c92e45d6b1abe7caa05f025
Rhena Mae Nalzaro <bossnami@Rhenas-MacBook-Air.local>
2026-05-12 01:12:01 +0800

    Update UM-Pasa system

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'url' => rtrim(env('APP_URL', 'https://your-domain.example'), '/').'/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
