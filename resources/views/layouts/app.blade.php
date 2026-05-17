6d27757387071cf65009d27f233337ae5e6f0664
Rhena Mae Nalzaro <bossnami@Rhenas-MacBook-Air.local>
2026-05-18 01:06:11 +0800

    Add UM-Pasa favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="dark">
     <head>
+        <title>UM-Pasa | University Marketplace</title>
+
+        <link rel="icon" type="image/png" href="{{ asset('UMPASALOGO.png') }}">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('UMPASALOGO.png') }}">
+        <link rel="apple-touch-icon" href="{{ asset('UMPASALOGO.png') }}">
+        <meta name="theme-color" content="#d71920">
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
